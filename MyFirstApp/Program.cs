6325d471c3fb25debaf08905d12284ead8278bab
Rafael <114652840+RafaelNagiyev@users.noreply.github.com>
2023-03-03 16:18:49 +0400

    Задание 5

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,11 +1,19 @@
-﻿namespace MyFirstApp
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            //папрапрарпа
-            Console.WriteLine("Hello, World!!!!");
-        }
-    }
-}
+﻿string name;
+
+Console.WriteLine("What is your name?");
+name=Console.ReadLine();
+
+Console.WriteLine("Hello " + name);
+
+string surname;
+
+Console.WriteLine("What is your surname?");
+surname=Console.ReadLine();
+
+string email;
+
+Console.WriteLine("What is your email?");
+email=Console.ReadLine();
+
+Console.WriteLine("*"+name+"*" + "-" + "*"+surname+"*" + " " + "*"+email+"*");
+
